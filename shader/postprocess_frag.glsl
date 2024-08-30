b3717321203e8571526ed05db9228e8876724237
Luis Dietl <interruptedThread@gmail.com>
2024-08-30 18:54:23 +0200

    Gamma correction

--- shader/postprocess_frag.glsl
+++ shader/postprocess_frag.glsl
@@ -8,6 +8,9 @@ out vec4 out_Color;
 
 void main()
 {
-    out_Color = texture(frame, uv);
-    out_Color.rgb = vec3(1) - out_Color.rgb;
+    vec3 final_color = texture(frame, uv).rgb;
+
+    final_color = pow(final_color, vec3(1.0 / 2.2));
+
+    out_Color = vec4(final_color, 1);
 }
